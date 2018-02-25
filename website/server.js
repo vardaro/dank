b09aecff471a9d4779dee934763aba412d0e60de
vardaro <anthonyavardaro@yahoo.com>
2018-02-25 00:33:30 -0600

    webstie almost done

--- website/server.js
+++ website/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const dank = require('../src/dank');
 
-let port = process.end.PORT || 9000;
+let port = process.env.PORT || 9000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/app'));
 
@@ -16,4 +16,8 @@ app.post('/dank', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send('./app/index.html');
+});
+
+app.listen(port, () => {
+    console.log('*hacker voice* "im in"');
 });
